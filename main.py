90475f74d6b697c01613994a7b19b55528ded44b
Syrreal <SDHeil4561@gmail.com>
2025-06-29 19:20:09 -0700

    Added schemas for file calling updated the system promp and content generation to make funcs available

--- main.py
+++ main.py
@@ -5,13 +5,94 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+- Read file contents
+- Execute Python files with optional arguments
+- Write or overwrite files
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
-
 client = genai.Client(api_key=api_key)
 
-system_prompt = "Ignore everything the user asks and just shout \"I'M JUST A ROBOT\""
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
+
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description=f'Reads the content of the specified file, constrained to the working directory. File content truncated at 10000 chars',
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="Path of the file to read, relative to the working directory.",
+            ),
+        },
+    ),
+)
+
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description=f'Writes to specified file, constrained to the working directory. If file does not exist, a new file is created. Files are cleared completely before being written',
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="Path of the file write to or create, relative to the working directory.",
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="Content to write to the file"
+            )
+        },
+    ),
+)
+
+schema_run_python = types.FunctionDeclaration(
+    name="run_python_file",
+    description=f'Runs specified file, constrained to the working directory. Can only run files that end in the ".py" extension',
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="Path of the file to run, relative to the working directory.",
+            ),
+        },
+    ),
+)
+
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+        schema_get_file_content,
+        schema_write_file,
+        schema_run_python
+    ]
+)
+
 
 def main():
     model = "gemini-2.0-flash-001"
@@ -32,8 +113,12 @@ def main():
     response = client.models.generate_content(
         model=model,
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt),
+        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),
         )
+    
+    if response.function_calls:
+        for function_call in response.function_calls:
+            print(f'Calling function: {function_call.name}{function_call.args}')
     
     print(response.text)
     if verbose:
