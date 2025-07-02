d7ff0429c762c03385a78484d9903c7a25ceb199
Syrreal <SDHeil4561@gmail.com>
2025-07-01 20:37:27 -0700

    moved system prompt to config file

--- config.py
+++ config.py
@@ -1,5 +1,20 @@
 # Hard coded working dir to ensure AI agent operates within bounds
 WORKING_DIR = "./calculator"
+
 MODEL = "gemini-2.0-flash-001"
+
 # Max amount of characters allowed to be read from a file
 MAX_CHARS = 10000
+
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+- Read file contents
+- Execute Python files with optional arguments
+- Write or overwrite files
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
--- main.py
+++ main.py
@@ -7,20 +7,7 @@ from google.genai import types
 
 from call_function import call_function, available_functions
 
-from config import MODEL
-
-system_prompt = """
-You are a helpful AI coding agent.
-
-When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
-
-- List files and directories
-- Read file contents
-- Execute Python files with optional arguments
-- Write or overwrite files
-
-All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
-"""
+from config import MODEL, SYSTEM_PROMPT
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -51,7 +38,6 @@ def generate_content(model, contents, verbose=False):
 
 
 def main():
-    
 
     if len(sys.argv) < 2:
         print("error: prompt not provided")
