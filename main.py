fedf9a47d13342ed537ba2e9eddddd9c3b583891
Syrreal <SDHeil4561@gmail.com>
2025-07-01 21:12:16 -0700

    Rewrote generate_content to iterate over client responses

--- main.py
+++ main.py
@@ -9,35 +9,53 @@ from call_function import call_function, available_functions
 
 from config import MODEL, SYSTEM_PROMPT
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
+def generate_content(client, messages, verbose=False):
+    loop_countdown = 20
+    while loop_countdown:
+        loop_countdown -= 1
 
+        response = client.models.generate_content(
+            model=MODEL,
+            contents=messages,
+            config=types.GenerateContentConfig(tools=[available_functions], system_instruction=SYSTEM_PROMPT),
+            )
+        
+        if verbose:
+            print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+            print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
 
-def generate_content(model, contents, verbose=False):
-    response = client.models.generate_content(
-        model=model,
-        contents=contents,
-        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),
-        )
-    
-    content_response = []
-    if response.function_calls:
+        # If theres no function call the AI is done performing their task
+        if not response.function_calls:
+            return response.text
+
+        # Candidates contains the AIs function call request
+        if response.candidates:
+            messages.extend(map(lambda x: x.content, response.candidates))
+
+        # Gather a list of function call results
+        content_responses = []
         for function_call in response.function_calls:
             print(f'Calling function: {function_call.name}{function_call.args}')
-            result = call_function(function_call)
+            result = call_function(function_call, verbose)
             if result.parts[0].function_response.response:
                 if verbose:
                     print(f"-> {result.parts[0].function_response.response}")
                 
-                content_response.append(result.parts[0].function_response.response["result"])
+                content_responses.append(result.parts[0].function_response.response["result"])
             else:
-                raise RuntimeError(f"Function {function_call.name} had no result")
-    
-    return ("/n".join(content_response), response)
+                raise RuntimeError("Function call had no result")
+        if not content_responses:
+            raise RuntimeError("No function responses generated")
+        messages.extend(content_responses)
+        
+    raise RuntimeError("Failed to perform task in allotted time")
+        
 
 
 def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
     if len(sys.argv) < 2:
         print("error: prompt not provided")
@@ -53,21 +71,15 @@ def main():
         types.Content(role="user", parts=[types.Part(text=user_prompt)])
     ]
 
-    # Return tuple (response text, response object)
-    # Perform first task out of the loop to create variable :) 
-    response = generate_content(MODEL, messages, verbose)
-
-    loop_countdown = 19
-    while loop_countdown:
-        loop_countdown -= 1
-        
-    if response[1].text:
-        print(response[1].text)
-    print(response[0])
     if verbose:
         print(f'User prompt: {user_prompt}')
-        print(f'Prompt tokens: {response[1].usage_metadata.prompt_token_count}')
-        print(f'Response tokens: {response[1].usage_metadata.candidates_token_count}')
+    
+    response = generate_content(client, messages, verbose)
+    
+    # If anything is return it is a final result text, otherwise an error should have been raised
+    if response:
+        print("Final response:")
+        print(response)
 
 if __name__ == "__main__":
     main()
